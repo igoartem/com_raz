2244b62b42661e53729d5e7d62c1c2b6d593b389
mazahaker <ser9-9-9@ya.ru>
2015-03-23 00:41:30 +0600

    Загрузка данных в таблицу и не работающее удаление

--- Формы Сучкова/Tovari.Designer.cs
+++ Формы Сучкова/Tovari.Designer.cs
@@ -63,16 +63,16 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.Select = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-            this.PK = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Naimenovanie = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Category = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.P_category = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Cost = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Prodan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Owner = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Date_ot = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Date_end = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PK = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Garant = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Owner = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -95,16 +95,16 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Select,
-            this.PK,
             this.Naimenovanie,
             this.Category,
             this.P_category,
             this.Cost,
             this.Prodan,
+            this.Owner,
             this.Date_ot,
             this.Date_end,
-            this.Garant,
-            this.Owner});
+            this.PK,
+            this.Garant});
             this.dataGridView1.Location = new System.Drawing.Point(6, 19);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(847, 422);
@@ -290,8 +290,9 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(125, 30);
             this.button2.TabIndex = 3;
-            this.button2.Text = "Отправить в архив";
+            this.button2.Text = "Удалить\r\nОтправить в архив";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -404,15 +405,11 @@
             // 
             // Select
             // 
+            this.Select.FalseValue = "false";
             this.Select.HeaderText = "Выделить";
             this.Select.Name = "Select";
+            this.Select.TrueValue = "true";
             this.Select.Width = 60;
-            // 
-            // PK
-            // 
-            this.PK.HeaderText = "PK";
-            this.PK.Name = "PK";
-            this.PK.Visible = false;
             // 
             // Naimenovanie
             // 
@@ -441,6 +438,14 @@
             this.Prodan.HeaderText = "Статус";
             this.Prodan.Name = "Prodan";
             // 
+            // Owner
+            // 
+            this.Owner.HeaderText = "Выкуплен";
+            this.Owner.Name = "Owner";
+            this.Owner.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Owner.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Owner.Width = 60;
+            // 
             // Date_ot
             // 
             this.Date_ot.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
@@ -453,19 +458,16 @@
             this.Date_end.HeaderText = "Дата окончания";
             this.Date_end.Name = "Date_end";
             // 
+            // PK
+            // 
+            this.PK.HeaderText = "PK";
+            this.PK.Name = "PK";
+            // 
             // Garant
             // 
             this.Garant.HeaderText = "Осталось гарантии";
             this.Garant.Name = "Garant";
             this.Garant.Width = 60;
-            // 
-            // Owner
-            // 
-            this.Owner.HeaderText = "Выкуплен";
-            this.Owner.Name = "Owner";
-            this.Owner.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.Owner.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.Owner.Width = 60;
             // 
             // Tovari
             // 
@@ -536,15 +538,15 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.DataGridViewCheckBoxColumn Select;
-        private System.Windows.Forms.DataGridViewTextBoxColumn PK;
         private System.Windows.Forms.DataGridViewTextBoxColumn Naimenovanie;
         private System.Windows.Forms.DataGridViewTextBoxColumn Category;
         private System.Windows.Forms.DataGridViewTextBoxColumn P_category;
         private System.Windows.Forms.DataGridViewTextBoxColumn Cost;
         private System.Windows.Forms.DataGridViewTextBoxColumn Prodan;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Owner;
         private System.Windows.Forms.DataGridViewTextBoxColumn Date_ot;
         private System.Windows.Forms.DataGridViewTextBoxColumn Date_end;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PK;
         private System.Windows.Forms.DataGridViewTextBoxColumn Garant;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Owner;
     }
 }
--- Формы Сучкова/Tovari.cs
+++ Формы Сучкова/Tovari.cs
@@ -7,14 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OracleClient;
 
 namespace Формы_Сучкова
 {
     public partial class Tovari : Form
     {
+        OracleCommand cmd1;
+        OracleConnection con1;
+        OracleDataReader dr1;
+        bool load;
+        int count = 0;
+
         public Tovari()
         {
+            load = true;
             InitializeComponent();
+        }
+
+        public void refresh()
+        {
+            cmd1.CommandText = "SELECT product.name, category.name, subcategory.name, product.EXPECT_PRICE, status.NAME, product.FLAG_OWNER, input_act.DATE_INP, input_act.DATE_END, product.PK_PROD FROM input_act, product, status, category, subcategory where product.PK_SUBCAT = subcategory.PK_SUBCAT and subcategory.PK_CAT = category.PK_CAT and product.PK_STAT = status.PK_STAT and product.PK_ACT = input_act.PK_ACT ";
+
+            dr1 = cmd1.ExecuteReader();
+
+            int i = 0;
+
+            dataGridView1.Enabled = false;
+            while (dr1.Read())
+            {
+                dataGridView1.Rows.Add();
+                for (int j = 0; j < 10; j++)
+                {
+                    if (j == 9)
+                    {
+                        //тут считаем колво дней остаток
+                    }
+                    else
+                        if (j == 5)
+                        {
+                            if (Convert.ToInt32(dr1[j].ToString()) == 0)
+                                dataGridView1.Rows[i].Cells[j + 1].Value = "Нет"; // статус товара наш не наш
+                            else
+                                dataGridView1.Rows[i].Cells[j + 1].Value = "Да";
+                        }
+                        else
+                            dataGridView1.Rows[i].Cells[j + 1].Value = dr1[j].ToString();
+                }
+
+
+                i++;
+            }
+            count = dataGridView1.Rows.Count;
+            dataGridView1.Enabled = true;
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -49,7 +94,15 @@ namespace Формы_Сучкова
 
         private void Tovari_Load(object sender, EventArgs e)
         {
+            load = true;
 
+            con1 = new OracleConnection("Data Source=(DESCRIPTION =(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 127.0.0.1)(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = xe))); User Id=" + "admin" + ";Password=" + "123" + ";");
+            cmd1 = new OracleCommand("", con1);
+            con1.Open();
+
+            refresh();
+
+            load = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -62,6 +115,19 @@ namespace Формы_Сучкова
         {
             Kategory kategory = new Kategory();
             kategory.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Удаление
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[0].Value.ToString() == "true")
+                {
+                    cmd1.CommandText = "delete from product where product.pk_prod = " + dataGridView1.Rows[i].Cells[0].Value.ToString();
+                    refresh();
+                }
+            }
         }
     }
 }
