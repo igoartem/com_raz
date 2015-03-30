21c93cca52e7efa51b679632b03a5b094f31f367
Laptev Mikhail <bugi-wugi93@mail.ru>
2015-03-30 19:20:30 +0700

    характеристика

---
+++ Формы Сучкова/Characteristic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Формы_Сучкова
+{
+    class Characteristic
+    {
+        string name;
+        int pk_char;
+        int pk_subcat;
+
+
+
+        public Characteristic(string name, int pk_subcat)
+        {
+            this.name = name;
+            this.pk_subcat = pk_subcat;
+
+        }
+
+
+        public Characteristic(string name, int pk_subcat, int pk_char)
+        {
+            this.name = name;
+            this.pk_subcat = pk_subcat;
+            this.pk_char = pk_char;
+        }
+
+    }
+
+
+
+
+
+
+}
